36bd55b7b84bf7d8d9d27177291f2e33d74284da
HuyLieu <huylieu163@gmail.com>
2024-10-24 04:48:36 +0700

    update

---
+++ src/services/auth.service.js
@@ -0,0 +1,20 @@
+import { date } from 'joi';
+import User from '../models/user.model';
+
+// Check email is already exist
+export const checkEmailExist = async (email)=>{
+    const user = await User.findOne({email});
+    return user;
+}
+
+// Create User
+export const createUser = async (data)=>{
+    const user = await User.create({data});
+    return user;
+};
+
+// Update Password
+export const updatePassword=async (userID,password)=>{
+    const user = User.findByIdAndUpdate({ _id : userID },{password},{new:true});
+    return Boolean(user);
+};
